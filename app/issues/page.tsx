2044f0740d7f14a5b19d62ebefaa127367bb16fe
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-01-28 19:51:27 -0400

    build a new Issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 const IssuesPage = () => {
   return (
-    <div>
-      Issues Page
-    </div>
+    <Button><Link href='./issues/new'>New Issue</Link></Button>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 import { Button } from '@radix-ui/themes'
 export default function Home() {
   return (
-    <div><Button>New Issue</Button> </div>
+    <div>HEllo World! </div>
   )
 }

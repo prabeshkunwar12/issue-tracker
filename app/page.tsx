be31dec6e1b9889011bfe39a6e2f084d16fb0562
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-01-28 19:40:21 -0400

    setup radix ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-
+import { Button } from '@radix-ui/themes'
 export default function Home() {
   return (
-    <div>Hello World!</div>
+    <div><Button>New Issue</Button> </div>
   )
 }

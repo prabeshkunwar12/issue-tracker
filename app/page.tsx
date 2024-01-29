c79d1634ec217d38fdb0601374079332ca593602
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-01-29 15:11:20 -0400

    only have build files

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-import { Button } from '@radix-ui/themes'
-export default function Home() {
-  return (
-    <div>HEllo World! </div>
-  )
-}
--- app/validationSchemas.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const createIssueSchema = z.object({
-    title: z.string().min(1, 'title is required').max(255),
-    description: z.string().min(1, 'description is required')
-});
